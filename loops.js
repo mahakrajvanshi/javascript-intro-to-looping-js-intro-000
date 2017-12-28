b79cfcacd1c40ec34a3b3c95e917e4cc3a1c0072
MAHAK RAJVANSHI <mahakrajvanshi@yahoo.com>
2017-12-28 23:22:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,10 +22,8 @@ function doWhileLoop(array){
   function maybeTrue() {
     return Math.random() >= 0.5
   }
-  
+
   do {
     array = array.slice(1)
   } while (array.length > 0 && maybeTrue() );
 }
-
-
